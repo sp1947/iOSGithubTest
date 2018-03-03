616f6b0512caa322e3e85c9e3b30d2f8b9701a12
TAE <saurabh.patel@tec20.co.uk>
2018-03-03 12:24:21 +0000

    test add function 2

---
+++ iOSGithubTest/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  iOSGithubTest
+//
+//  Created by TAE on 03/03/2018.
+//  Copyright © 2018 TAE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tottalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func addAction(_ sender: Any) {
+        let total = Double(firstTextField.text!)! + Double(secondTextField.text!)!
+
+        tottalLabel.text = String(format:"%f", total)
+    }
+    
+
+}
+
